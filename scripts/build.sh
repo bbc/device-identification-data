9c0159efdbc9e0d32ff8e00b373afb820060e95a
Alex Chesters <AlexChesters@users.noreply.github.com>
2017-02-20 13:17:29 +0000

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,3 +4,4 @@ set -e
 mkdir -p ./output
 npm install
 ./scripts/concat-devices.js "./devices/**/*.json" "./output/device-identification-data.json"
+aws s3 cp data/make-and-model.csv s3://connected-tv-public-unversioned/device-identification-data/data.json --cache-control max-age=60 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
