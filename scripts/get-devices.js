ad59eb44b3af3a246f42279b2fc6f767c8626154
Adam Cook <adamjc@users.noreply.github.com>
2017-02-21 09:31:34 +0000

    Refactor testing and minifying (#3)
    * Concat & minify device data
    
    * Lint whitespace
    
    * Correctly pass through devices for testing schema
    
    * Refactor how we test schemas and build
    
    * We never reject this promise
    
    * Fix weird merge weirdness
    
    * Add npm-debug.log to .gitignore
    
    * Fix schema tests and update schema for PE5

---
+++ scripts/get-devices.js
@@ -0,0 +1,21 @@
+const glob = require('glob')
+const path = require('path')
+const fs = require('fs')
+
+function getDevices (directory) {
+  return new Promise(resolve => {
+    let devices = {}
+
+    glob(directory, (err, files) => {
+      files.forEach(file => {
+        const contents = fs.readFileSync(file, 'utf-8')
+        const device = path.basename(file, '.json')
+        devices[device] = JSON.parse(contents)
+      })
+
+      resolve(devices)
+    })
+  })
+}
+
+module.exports = getDevices
