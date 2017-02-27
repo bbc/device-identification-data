8e0c0084e141e7fcec21bb35bdf4343741c6854d
Adam Cook <Adam.Cook@bbc.co.uk>
2017-02-27 11:17:06 +0000

    Change contents from const -> let - Internally the state is changing, so semantically it should be let

--- scripts/get-devices.js
+++ scripts/get-devices.js
@@ -8,7 +8,7 @@ function getDevices (directory) {
 
     glob(directory, (err, files) => {
       files.forEach(file => {
-        const contents = JSON.parse(fs.readFileSync(file, 'utf-8'))
+        let contents = JSON.parse(fs.readFileSync(file, 'utf-8'))
         const device = path.basename(file, '.json').split('-')
         contents.brand = device[0]
         contents.model = device[1]
