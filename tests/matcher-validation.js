22e014bf5d4ac3e097edd04af8998ccaf250fa3b
Alex Chesters <alex.chesters@bbc.co.uk>
2017-03-01 08:34:44 +0000

    Split out function

--- tests/matcher-validation.js
+++ tests/matcher-validation.js
@@ -17,30 +17,46 @@ function parse (data) {
   })
 }
 
+function testLine (line) {
+  const [brand, model, ua] = line
+
+  const device = match(ua)
+
+  const outputDevices = output.filter((matcher) => matcher.brand === brand && matcher.model === model)
+
+  if (outputDevices.length) {
+    const outputBrand = outputDevices[0].brand
+    const outputModel = outputDevices[0].model
+
+    if (device.brand !== outputBrand || device.model !== outputModel) {
+      return {
+        expected: { brand, model },
+        actual: { brand: outputBrand, model: outputModel },
+        ua: ua
+      }
+    }
+  }
+}
+
+function logFailure (failure) {
+  const {expected, actual, ua} = failure
+  console.error(`
+    Bad match: ${expected.brand} ${expected.model}
+    Expected to match: ${actual.brand} ${actual.model}
+    UA: ${ua}
+  `)
+}
+
 testDataFetch.then((response) => {
   return response.text()
 }).then((testData) => {
   return parse(testData)
 }).then((testDataLines) => {
-  testDataLines.forEach((testDataLine) => {
-    const [brand, model, ua] = testDataLine
-
-    const device = match(ua)
-
-    const outputDevices = output.filter((matcher) => matcher.brand === brand && matcher.model === model)
-
-    if (outputDevices.length) {
-      const outputBrand = outputDevices[0].brand
-      const outputModel = outputDevices[0].model
-
-      if (device.brand !== outputBrand || device.model !== outputModel) {
-        console.error(
-          `Bad match: ${outputBrand} ${outputModel}`,
-          `Expected to match: ${brand} ${model}`,
-          `UA: ${ua}`
-        )
-        process.exit(1)
-      }
-    }
-  })
+  const results = testDataLines.map(testLine)
+  return results.filter(item => item !== undefined)
+}).then((failures) => {
+  return failures.forEach(logFailure)
+}).catch((error) => {
+  console.error(error)
+  process.exit(1)
 })
