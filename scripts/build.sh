7b3a15df6e4ed97bec73a52b5ec4d9589f52af62
Alex Chesters <alex.chesters@bbc.co.uk>
2017-03-01 08:38:58 +0000

    Split build and deploy scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,3 @@ set -e
 mkdir -p ./output
 npm install
 ./scripts/concat-devices.js "./devices/**/*.json" "./output/device-identification-data.json"
-if [ "$BUILD_NUMBER" ]; then
-  aws s3 cp output/device-identification-data.json s3://connected-tv-public-unversioned/device-identification-data/data.json --cache-control max-age=60 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-fi
