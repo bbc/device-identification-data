5d5652fa4299f2d9c7600b918f2579493cbe4fad
Adam Walters <adam.walters@bbc.co.uk>
2017-03-23 09:34:24 +0000

    added 'type: 'tv'' to all devices

--- scripts/device-type.js
+++ scripts/device-type.js
@@ -1,4 +1,4 @@
-#!usr/bin/env node
+#!/usr/bin/env node
 
 const fs = require('fs');
 
@@ -8,5 +8,9 @@ deviceBrands.forEach(function(brand) {
     var deviceModels = fs.readdirSync(`devices/${brand}`);
     deviceModels.forEach(function(model) {
       var deviceData = JSON.parse(fs.readFileSync(`devices/${brand}/${model}`, 'utf-8'));
+      deviceData.forEach(function(device) {
+        device.type = 'tv';
+          fs.writeFileSync(`devices/${brand}/${model}`, JSON.stringify(device, null, 2));
+      })
     })
 })
